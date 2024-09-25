a15e035808b4998e4b4d1e7f2775e7b45025fe49
Gabriela Machado <gbms4078@gmail.com>
2024-09-25 14:50:27 -0500

    subir

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -4,6 +4,8 @@
  */
 package javaapplication5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author JAZIEL
@@ -14,7 +16,45 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+      Sca2ner scanner = new Scanner(System.in);
+
+        // Declaración de los arreglos
+        int[] arreglo1 = new int[7];
+        int[] arreglo2 = new int[7];
+        int[] arregloDiferencia = new int[7];
+        int suma = 0;
+
+        // Llenar arreglo1
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Ingrese el número para arreglo1 en la posición " + i + ":");
+            arreglo1[i] = scanner.nextInt();
+        }
+
+        // Llenar arreglo2
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Ingrese el número para arreglo2 en la posición " + i + ":");
+            arreglo2[i] = scanner.nextInt();
+        }
+
+        // Llenar el tercer arreglo con la diferencia y calcular la suma
+        for (int i = 0; i < 7; i++) {
+            arregloDiferencia[i] = arreglo1[i] - arreglo2[i];
+            suma += arregloDiferencia[i];
+        }
+
+        // Calcular el promedio
+        double promedio = (double) suma / 7;
+
+        // Mostrar todos los valores del tercer arreglo
+        System.out.println("Valores del tercer arreglo (diferencia):");
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Posición " + i + ": " + arregloDiferencia[i]);
+        }
+
+        // Mostrar el promedio
+        System.out.println("El promedio de los valores del tercer arreglo es: " + promedio);
+    
     }
     
 }
